caee42149d6e82fe3c1f5a4cfa218aa15ccea5a7
hanum <rntls123@naver.com>
2023-02-21 01:15:40 +0900

    connect socket

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,21 @@
+import WebSocket from "ws";
+import http from "http";
 import express from "express";
 
 const PORT: number = 3000;
 const app: express.Application = express();
 
-// app.set("view engine", "pug");
-// app.set("views", __dirname + "/public/views");
-// app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => res.sendStatus(200));
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+const server = http.createServer(app); // http 서버
+const wss = new WebSocket.Server({ server }); // ws(websocket) 서버
+// 이렇게 함으로써 동일 포트에 http / ws 서버를 함께 구동
+// 필수 사항은 아니며 ws 서버만 구동해도 무관
+
+function handleConnection(socket: WebSocket, request: http.IncomingMessage) {
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
+server.listen(PORT, () =>
+  console.log(`Listening on PORT: http://localhost:${PORT}`)
+);
