f70c0b8ac8a19c60bca1f200a2a0fc6a77fa143f
hanum <rntls123@naver.com>
2023-02-21 02:08:09 +0900

    send a message to other socket

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,11 +12,16 @@ const wss = new WebSocket.Server({ server }); // ws(websocket) 서버
 // 이렇게 함으로써 동일 포트에 http / ws 서버를 함께 구동
 // 필수 사항은 아니며 ws 서버만 구동해도 무관
 
+const sockets: WebSocket[] = []; // this will be fake DB
+
 wss.on("connection", (socket: WebSocket, request: http.IncomingMessage) => {
   console.log("connected --- O");
+  sockets.push(socket);
   socket.send("hello~");
   socket.on("message", (message: MessageEvent) => {
-    console.log(message.toString());
+    sockets.forEach((socket) => {
+      socket.send(message.toString());
+    });
   });
   socket.on("close", () => {
     console.log("disconnected --- X");
