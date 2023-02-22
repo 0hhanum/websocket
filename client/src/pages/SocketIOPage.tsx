f729b44c92978970f73079f2536e57dd45443de9
hanum <rntls123@naver.com>
2023-02-22 23:56:53 +0900

    socket.io

--- client/src/pages/SocketIOPage.tsx
+++ client/src/pages/SocketIOPage.tsx
@@ -1,64 +1,7 @@
 import { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-interface IForm {
-  message: string;
-  nickname: string;
-}
 function SocketIOPage() {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [connect, setConnect] = useState(false);
-  const [messages, setMessages] = useState<string[]>([]);
-  const { register, handleSubmit, resetField } = useForm<IForm>();
-  const onValid: SubmitHandler<IForm> = ({ message, nickname }) => {
-    socket?.send(
-      JSON.stringify({
-        message: message,
-        nickname: nickname,
-      })
-    );
-    setMessages((current) => current.concat(`Me: ${message}`));
-    resetField("message");
-  };
-  useEffect(() => {
-    const socket: WebSocket = new WebSocket("ws://localhost:3000");
-    setSocket(socket);
-    socket.addEventListener("open", () => {
-      setConnect(true);
-    });
-    socket.addEventListener("message", (message: MessageEvent) => {
-      setMessages((current) => current.concat(`someone: ${message.data}`));
-    });
-  }, []);
-  return (
-    <>
-      {connect ? (
-        <div>
-          <h2>Connected to Server</h2>
-          <ul>
-            {messages.map((message) => (
-              <li>{message}</li>
-            ))}
-          </ul>
-          <form onSubmit={handleSubmit(onValid)}>
-            <div style={{ display: "flex" }}>
-              <input
-                {...register("nickname", { required: true })}
-                type="text"
-                placeholder="choose a nickname"
-                style={{ width: "20%", marginRight: "20px" }}
-              />
-              <input
-                {...register("message", { required: true })}
-                type="text"
-                placeholder="write a message"
-              />
-            </div>
-            <button>Send</button>
-          </form>
-        </div>
-      ) : null}
-    </>
-  );
+  return <></>;
 }
 export default SocketIOPage;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import WebSocket, { MessageEvent } from "ws";
 import http from "http";
 import express from "express";
-
+import { Server as SocketIO } from "socket.io";
 const PORT: number = 3000;
 interface IData {
   message: string;
@@ -14,14 +14,14 @@ const app: express.Application = express();
 
 app.get("/", (req, res) => res.sendStatus(200));
 
-const server = http.createServer(app); // http 서버
+const httpServer = http.createServer(app); // http 서버
 
-const wss = new WebSocket.Server({ server }); // ws(websocket) 서버
+const wsServer = new WebSocket.Server({ server: httpServer }); // ws(websocket) 서버
 // 이렇게 함으로써 동일 포트에 http / ws 서버를 함께 구동
 // 필수 사항은 아니며 ws 서버만 구동해도 무관
 
 const sockets: ISocket[] = []; // this will be fake DB
-wss.on("connection", (socket: ISocket, request: http.IncomingMessage) => {
+wsServer.on("connection", (socket: ISocket, request: http.IncomingMessage) => {
   console.log("connected --- O");
   socket["nickname"] = "Anonymous";
   sockets.push(socket);
@@ -41,6 +41,7 @@ wss.on("connection", (socket: ISocket, request: http.IncomingMessage) => {
   });
 });
 
-server.listen(PORT, () =>
+const ioServer = new SocketIO(httpServer);
+httpServer.listen(PORT, () =>
   console.log(`Listening on PORT: http://localhost:${PORT}`)
 );
